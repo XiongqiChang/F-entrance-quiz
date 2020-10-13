38a77f1451b740d1e0da440218053e8eb04b9f3a
XiongqiChang <2838159948@qq.com>
2020-10-13 17:42:49 +0800

    前后端联合出问题

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.scss';
 import StudentList from '../components/StudentList';
+import GroupList from '../components/GroupList';
 
 class App extends Component {
   render() {
     return (
       <div data-testid="app" className="App">
         Hello World
+        <GroupList />
         <StudentList />
       </div>
     );
--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -4,4 +4,34 @@ export default class StudentList extends Component {
   render() {
     return <div />;
   }
+  /* 
+    state = {
+        id: '',
+        name: '',
+    }
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    async componentWillMount() {
+        let students = await (await fetch('http://localhost:8080/students')).json();
+        this.setState({ students });
+    }
+    
+    render() {
+        let { students = [] } = this.state;
+        return (<div>学员列表
+            {students.map(({ id, name }) =>
+                <div >
+                    <p>{id}</p>
+                    <p>{name}</p>
+                </div>
+            )}
+        </div>
+        );
+    } 
+    */
 }
