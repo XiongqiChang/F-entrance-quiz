9b487514ee48512889ed8918bba7734b17230f98
kevindongzg <dongkevin94@gmail.com>
2020-10-16 01:13:27 +0800

    add TODOs

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -4,34 +4,34 @@ export default class StudentList extends Component {
   render() {
     return <div />;
   }
-  /* 
-    state = {
-        id: '',
-        name: '',
-    }
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-        }
-    }
+  // TODO state只写在一个地方，这里或者constructor里面，且内容不对
+  state = {
+    id: '',
+    name: '',
+  };
 
-    async componentWillMount() {
-        let students = await (await fetch('http://localhost:8080/students')).json();
-        this.setState({ students });
-    }
-    
-    render() {
-        let { students = [] } = this.state;
-        return (<div>学员列表
-            {students.map(({ id, name }) =>
-                <div >
-                    <p>{id}</p>
-                    <p>{name}</p>
-                </div>
-            )}
-        </div>
-        );
-    } 
-    */
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  async componentWillMount() {
+    const students = await (await fetch('http://localhost:8080/students')).json();
+    this.setState({ students });
+  }
+
+  render() {
+    const { students = [] } = this.state;
+    return (
+      <div>
+        学员列表
+        {students.map(({ id, name }) => (
+          <div>
+            <p>{id}</p>
+            <p>{name}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
